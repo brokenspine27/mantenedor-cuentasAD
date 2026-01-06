60a48f2aa02a645fc689d4a0e5a06b88dc60684f
brokenspine27 <doris.ilabaca@inacapmail.cl>
2026-01-06 09:46:58 -0300

    importar environ

--- ad_conciliacion/settings.py
+++ ad_conciliacion/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+#importacion del environ
+env = environ.Env() #crear un lector
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
